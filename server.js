7e58787c0ef67880baf9d60dd489bf2ac6c645bb
Miles Au <miles.d.au@gmail.com>
2019-10-18 01:21:58 -0400

    heroku connection string

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
   dialect: 'pg',
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true
   },
 });
